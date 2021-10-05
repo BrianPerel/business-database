7368931a0f8f1a61c60d332cce64aaca940b5f20
Brian Perel <brianperel@comcast.net>
2021-10-05 09:19:55 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,45 +1,45 @@
-SELECT * FROM CUSTOMERS;
+SELECT * FROM customers;
 
-SELECT * FROM ITEM
-WHERE Item_number = ( SELECT Item_number FROM Orders
-WHERE Customer_number = ( SELECT Customer_number
-FROM Customers WHERE Fname = 'Tom' AND Lname = 'Brady'));
+SELECT * FROM items
+WHERE item_number = ( SELECT item_number FROM orders
+WHERE customer_number = ( SELECT customer_number
+FROM customers WHERE fname = 'Tom' AND lname = 'Brady'));
 
-SELECT Order_number FROM Orders
-WHERE Employee_number = ( SELECT Employee_number   
-FROM Employees WHERE Fname = 'Tony' AND Lname = 'Romo');
+SELECT order_number FROM orders
+WHERE employee_number = ( SELECT employee_number   
+FROM employees WHERE fname = 'Tony' AND lname = 'Romo');
 
-SELECT Fname, Lname
-FROM Customers
-WHERE Customer_number in ( SELECT Customer_number
-FROM Orders WHERE Item_number in ( SELECT Item_number
-FROM Item WHERE price > 30.00));
+SELECT fname, lname
+FROM customers
+WHERE customer_number in ( SELECT customer_number
+FROM orders WHERE item_number in ( SELECT item_number
+FROM item WHERE price > 30.00));
 
-SELECT Fname, Lname, City
-FROM Employees, Item, Orders
-WHERE Item.price > 50
-AND Employees.Employee_number = Orders.Employee_number
-AND Item.Item_number = Orders.Item_number;
+SELECT fname, lname, city
+FROM employees, item, orders
+WHERE item.price > 50
+AND employees.employee_number = orders.employee_number
+AND item.item_number = orders.item_number;
 
-SELECT Customers.Fname, Customers.Lname
-FROM Customers, Employees, Orders
-WHERE Customers.City = Employees.City
-AND Orders.Employee_number = Employees.Employee_number
-AND Orders.Customer_number = Customers.Customer_number;
+SELECT customers.fname, customers.lname
+FROM customers, employees, orders
+WHERE customers.city = employees.city
+AND orders.employee_number = employees.employee_number
+AND orders.customer_number = customers.customer_number;
 
-SELECT Fname, Lname
-FROM Customers, Orders
-WHERE Orders.Ship_date = Orders.ship_date
-AND Orders.Customer_number = Customers.Customer_number;
+SELECT fname, lname
+FROM customers, orders
+WHERE orders.ship_date = orders.ship_date
+AND orders.customer_number = customers.customer_number;
 
-SELECT Fname, Lname
-FROM Customers
-WHERE Customer_number
-NOT IN(SELECT Customer_number FROM Orders);
+SELECT fname, lname
+FROM customers
+WHERE customer_number
+NOT IN(SELECT customer_number FROM orders);
 
-SELECT i.Item_number, i.Item_name, sum (o.quantity) FROM Item i inner JOIN Orders o ON i.Item_number = o.Item_number 
+SELECT i.item_number, i.item_name, sum (o.quantity) FROM item i inner JOIN orders o ON i.item_number = o.item_number 
 GROUP BY i.item_number, i.item_name;
 
-SELECT e.Employee_number, e.Fname, e.Lname, sum (i.price * o.quantity) AS Totalsales FROM Employees e 
-inner JOIN Orders o ON e.Employee_number = o.Employee_number
-inner JOIN Item i ON o.Item_number = i.Item_number GROUP BY e.Employee_number, e.Fname, e.Lname;
+SELECT e.employee_number, e.fname, e.lname, sum (i.price * o.quantity) AS totalsales FROM employees e 
+inner JOIN orders o ON e.employee_number = o.employee_number
+inner JOIN item i ON o.item_number = i.item_number GROUP BY e.employee_number, e.fname, e.lname;
