ee2b61ed8d6f8f37535666f92af6a0f7c065f4c4
Brian Perel <brianperel@comcast.net>
2021-10-05 09:13:12 -0400

    Update InsertTables.sql

--- InsertTables.sql
+++ InsertTables.sql
@@ -2,12 +2,12 @@
 -- TABLE: employees
 --
 CREATE TABLE employees (
-Employee_number int,
+employee_number INT,
 fName varchar(10) NOT NULL,
 lName varchar(20) NOT NULL,
 city varchar(20) NOT NULL,
 state varchar(2) NOT NULL,
-PRIMARY KEY (Employee_number)
+PRIMARY KEY (employee_number)
 );
 
 
@@ -15,12 +15,12 @@ PRIMARY KEY (Employee_number)
 -- TABLE: customers
 --
 CREATE TABLE customers (
-Customer_number int,
+customer_number INT,
 fName varchar(10) NOT NULL,
 lName varchar(20) NOT NULL,
 city varchar(20) NOT NULL,
 state varchar(2) NOT NULL,
-PRIMARY KEY (Customer_number)
+PRIMARY KEY (customer_number)
 );
 
 
@@ -28,27 +28,27 @@ PRIMARY KEY (Customer_number)
 -- TABLE: item
 --
 CREATE TABLE item (
-Item_number int,
+item_number INT,
 item_name varchar(80) NOT NULL,
 price decimal(10,2) NOT NULL,
-Stock_quantity int,
-PRIMARY KEY (Item_number)
+stock_quantity INT,
+PRIMARY KEY (item_number)
 );
 
 --
 -- TABLE: orders
 --
 CREATE TABLE orders (
-Order_number int,
-Customer_number int,
-Employee_number int,
-Item_number int,
-quantity int,
-Date_of_Receipt int,
-Ship_date date,
-PRIMARY KEY (Order_number),
-CONSTRAINT FK_CustomerOrder FOREIGN KEY (Customer_number) REFERENCES customers(Customer_number),
-CONSTRAINT FK_EmployeeOrder FOREIGN KEY (Employee_number) REFERENCES employees(Employee_number),
-CONSTRAINT FK_ItemOrder FOREIGN KEY (Item_number) REFERENCES item(Item_number)
+order_number INT,
+customer_number INT,
+employee_number INT,
+item_number INT,
+quantity INT,
+date_of_receipt INT,
+ship_date DATE,
+PRIMARY KEY (order_number),
+CONSTRAINT FK_CustomerOrder FOREIGN KEY (customer_number) REFERENCES customers(customer_number),
+CONSTRAINT FK_EmployeeOrder FOREIGN KEY (employee_number) REFERENCES employees(employee_number),
+CONSTRAINT FK_ItemOrder FOREIGN KEY (item_number) REFERENCES item(item_number)
 );
 
